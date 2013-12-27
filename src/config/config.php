78ad7edcf1c4e612b37471c25cf34f5a2ff26d90
Andrew Judd <awjudd@gmail.com>
2013-12-27 09:57:15 -0500

    Adding in a configuration file

---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    /**
+     * Contains all of the namespaces in order that the view should look at
+     * before actually rendering.
+     */
+    'namespaces' => array (
+        /*
+         * Should always keep a blank one here to search in no namespaces.
+         */
+        '',
+    ),
+);
