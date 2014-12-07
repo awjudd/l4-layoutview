7334809faaedbd22dfcbb03fdaa00ceb2f402727
Andrew Judd <awjudd@gmail.com>
2014-12-06 22:59:45 -0500

    Code cleanup and fixes
     - Fixed the indentation/spaces issue
     - Fixing the issue that was caused by the core ServiceProvider no longer providing a `package` function

--- src/Awjudd/Layoutview/LayoutviewServiceProvider.php
+++ src/Awjudd/Layoutview/LayoutviewServiceProvider.php
@@ -5,14 +5,14 @@ use Illuminate\Support\ServiceProvider;
 class LayoutviewServiceProvider extends ServiceProvider
 {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
+    /**
      * Bootstrap the service provider.
      *
      * @return void
@@ -22,43 +22,43 @@ class LayoutviewServiceProvider extends ServiceProvider
         $this->package('awjudd/layoutview');
     }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->bindShared('view', function($app)
-		{
-			// Next we need to grab the engine resolver instance that will be used by the
-			// environment. The resolver will be used by an environment to get each of
-			// the various engine implementations such as plain PHP or Blade engine.
-			$resolver = $app['view.engine.resolver'];
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('view', function($app)
+        {
+            // Next we need to grab the engine resolver instance that will be used by the
+            // environment. The resolver will be used by an environment to get each of
+            // the various engine implementations such as plain PHP or Blade engine.
+            $resolver = $app['view.engine.resolver'];
 
-			$finder = $app['view.finder'];
+            $finder = $app['view.finder'];
 
-			$env = new LayoutView($resolver, $finder, $app['events']);
+            $env = new LayoutView($resolver, $finder, $app['events']);
 
-			// We will also set the container instance on this view environment since the
-			// view composers may be classes registered in the container, which allows
-			// for great testable, flexible composers for the application developer.
-			$env->setContainer($app);
+            // We will also set the container instance on this view environment since the
+            // view composers may be classes registered in the container, which allows
+            // for great testable, flexible composers for the application developer.
+            $env->setContainer($app);
 
-			$env->share('app', $app);
+            $env->share('app', $app);
 
-			return $env;
-		});
-	}
+            return $env;
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('view');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('view');
+    }
 
 }
